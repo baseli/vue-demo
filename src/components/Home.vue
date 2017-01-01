32b13a03b6f66fa07b78924aa9ad8d54ea8c5b9a
liwd <liwd@liwddeMacBook-Pro.local>
2017-01-01 15:43:07 +0800

    引入echarts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,9 +6,7 @@
                 <span style="line-height: 36px;">卡片名称</span>
                 <el-button style="float: right;" type="primary">操作按钮</el-button>
               </div>
-              <div v-for="o in 4" class="text item">
-                {{'列表内容 ' + o }}
-              </div>
+              <div id="test" @click=test()></div>
             </el-card>
         </el-col>
         <el-col :span=8>
@@ -37,12 +35,45 @@
 </template>
 
 <script>
+import Ajax from '../ajax.js'
 export default {
+    mounted: function() {
+        Ajax.login({}, function(res) {
+            alert(res)
+        })
+        
+        var echarts = require('echarts');
+
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('test'));
+        // 绘制图表
+        myChart.setOption({
+            title: { text: 'ECharts 入门示例' },
+            tooltip: {},
+            xAxis: {
+                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
+    },
+    methods: {
+        test: function() {
+            alert(1);
+        }
+    }
 }
 </script>
 
 <style lang="css">
 .el-card__header {
     padding: 10px;
+}
+#test {
+    height: 600px;
 }
 </style>
